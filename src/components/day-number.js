9ec709d26bcab566c6f6c63e62e8769601aff5d8
AlexyShu <Za9i@bk.ru>
2020-05-01 15:22:14 +0300

    fixed

--- src/components/day-number.js
+++ src/components/day-number.js
@@ -1,6 +1,5 @@
 import {months} from '../mocks/trip-info.js';
 import AbstractComponent from "./abstract-component.js";
-import {dateFormat} from '../utils/common.js';
 
 const createTripDayItemTemplate = (dayCount, startDate) => {
   let dayInfoTemplate = ``;
@@ -9,7 +8,7 @@ const createTripDayItemTemplate = (dayCount, startDate) => {
     const day = startRouteDate.getDate();
     const month = months[startRouteDate.getMonth()];
     dayInfoTemplate = `<span class="day__counter"> ${dayCount} </span>
-    <time class="day__date" datetime="${dateFormat(startDate)}"> ${month} ${day}</time>`;
+    <time class="day__date" datetime="${startDate}"> ${month} ${day}</time>`;
   }
   return (`<li class="trip-days__item  day">
       <div class="day__info">
